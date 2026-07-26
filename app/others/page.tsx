73eeb4c1f2f98cd3d1e2f7e986f1f240a56652cb
Hung Tran <tranndhungg@gmail.com>
2026-07-26 15:18:28 -0700

    Update page.tsx

--- app/others/page.tsx
+++ app/others/page.tsx
@@ -44,15 +44,15 @@ export default function Visuals() {
                             <MyButton variant="blue-button-outline" text="View on official site &#8599;" link="https://www.fraserhealth.ca/-/media/Project/FraserHealth/FraserHealth/About-Us/Corporate-Brochure/Organizational-Brochure-2024_FINAL.pdf" textSize="text-sm" target="_blank"/>
                         </div>
                         <div className="flex flex-col gap-2">
-                            <div className=" bg-[var(--container-bg)] w-full flex justify-center items-center rounded-2xl px-15 py-15">
+                            <div className=" bg-[var(--container-bg)] w-full flex justify-center items-center rounded-3xl px-15 py-15">
                                 <ImageZoom>
-                                    <Image src="/fh-1.png" width={1200} height={1200} className="w-[100%] object-cover rounded-2xl shadow-[0px_0px_25px_rgba(0,0,0,0.035)]" alt="Visual design"/>
+                                    <Image src="/fh-1.png" width={1200} height={1200} className="w-[100%] object-cover rounded-xl shadow-[0px_0px_25px_rgba(0,0,0,0.035)]" alt="Visual design"/>
                                 </ImageZoom>
                             </div>
                             <div></div>
-                            <div className=" bg-[var(--container-bg)] w-full flex justify-center items-center rounded-2xl px-15 py-15">
+                            <div className=" bg-[var(--container-bg)] w-full flex justify-center items-center rounded-3xl px-15 py-15">
                                 <ImageZoom>
-                                    <Image src="/fh-2.png" width={1200} height={1200} className="w-[100%] object-cover rounded-2xl shadow-[0px_0px_25px_rgba(0,0,0,0.035)]" alt="Visual design"/>
+                                    <Image src="/fh-2.png" width={1200} height={1200} className="w-[100%] object-cover rounded-xl shadow-[0px_0px_25px_rgba(0,0,0,0.035)]" alt="Visual design"/>
                                 </ImageZoom>
                             </div>
                         </div>
