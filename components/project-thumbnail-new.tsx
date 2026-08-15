73b7b96fc2c157520c850ff88b0d15b07e0debdf
Hung Tran <tranndhungg@gmail.com>
2026-08-15 14:40:36 -0700

    idk

--- components/project-thumbnail-new.tsx
+++ components/project-thumbnail-new.tsx
@@ -2,12 +2,16 @@
 
 import Image from "next/image";
 import MyButton from "./my-button";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 interface Props {
     name: string;
     desc: string;
     imageSrc: string;
+    imageSrcDark: string;
     imageSrcPoster: string;
+    imageSrcPosterDark: string;
     link: string;
     target: string;
     isLink: string;
@@ -15,6 +19,15 @@ interface Props {
 }
 
 export default function ProjectThumbnailNew(props: Props) {
+    const { theme } = useTheme();
+    const [mounted, setMounted] = useState(false);
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+    if (!mounted) {
+        return null;
+    }
+    
     if (props.isLink == "image-link") {
         return (
             <div className="h-full flex flex-col group">
@@ -28,7 +41,23 @@ export default function ProjectThumbnailNew(props: Props) {
                 </div>
                 <div className="mb-3"></div>
                 <a className="relative overflow-hidden block group mt-auto bg-[var(--container-bg)] rounded-2xl shadow-[0px_0px_25px_rgba(0,0,0,0.015)] 2xl:shadow-[0px_0px_35px_rgba(0,0,0,0.01)] duration-300" href={props.link} target={props.target}>
-                    <Image src={props.imageSrc} alt={props.name} width={1640} height={1228} className="object-cover"/>
+                    {theme === "dark" ? (
+                        <Image
+                            src={props.imageSrcDark}
+                            alt={props.name}
+                            width={1640}
+                            height={1228}
+                            className="object-cover"
+                        />
+                    ) : (
+                        <Image
+                            src={props.imageSrc}
+                            alt={props.name}
+                            width={1640}
+                            height={1228}
+                            className="object-cover"
+                        />
+                    )}
                 </a>
             </div>
         );
@@ -46,9 +75,15 @@ export default function ProjectThumbnailNew(props: Props) {
                 </div>
                 <div className="mb-3"></div>
                 <a className="relative overflow-hidden block group mt-auto bg-[var(--container-bg)] rounded-2xl shadow-[0px_0px_25px_rgba(0,0,0,0.015)] 2xl:shadow-[0px_0px_35px_rgba(0,0,0,0.01)] duration-300" href={props.link} target={props.target}>
-                    <video className={`w-full h-full object-cover !bg-transparent block`} style={{ clipPath: "inset(-1px -1px)" }} autoPlay muted loop playsInline poster={props.imageSrcPoster}>
-                        <source src={props.imageSrc} type="video/mp4" />
-                    </video>
+                    {theme === "dark" ? (
+                        <video className={`w-full h-full object-cover !bg-transparent block`} style={{ clipPath: "inset(-1px -1px)" }} autoPlay muted loop playsInline poster={props.imageSrcPosterDark}>
+                            <source src={props.imageSrcDark} type="video/mp4" />
+                        </video>
+                    ) : (
+                        <video className={`w-full h-full object-cover !bg-transparent block`} style={{ clipPath: "inset(-1px -1px)" }} autoPlay muted loop playsInline poster={props.imageSrcPoster}>
+                            <source src={props.imageSrc} type="video/mp4" />
+                        </video>
+                    )}
                 </a>
             </div>
         );
